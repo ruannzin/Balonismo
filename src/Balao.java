db6e3564a94bec39f88f4baad5a2c2400e197978
ruannzin <ruanpablo24052006@gmail.com>
2023-06-06 08:29:53 -0300

    Fizemos um classe chamado Balão,e definimos os atributos

--- src/Balao.java
+++ src/Balao.java
@@ -1,3 +1,15 @@
 public class Balao {
-
+      // Atributos
+     int id;
+     String registro;
+     String cor;
+     String tipo;
+     int capacidade;
+     double altura;
+     String porte;
+     double peso;
+     //Métodos
+    public String voar(){
+        return "voando";
+    }
 }
